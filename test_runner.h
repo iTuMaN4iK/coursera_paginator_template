423f82f6559089b9a07418a03c5296a8965b86df
iTuMaN4iK <swaga.ituman4ik@gmail.com>
2020-07-14 00:47:32 +0500

    test_runner update

--- test_runner.h
+++ test_runner.h
@@ -93,11 +93,11 @@ private:
 
 #define ASSERT_EQUAL(x, y)                                                     \
   {                                                                            \
-    ostringstream os;                                                          \
-    os << #x << " != " << #y << ", " << __FILE__ << ":" << __LINE__;           \
-    AssertEqual(x, y, os.str());                                               \
+    ostringstream __assert_equal_private_os;                                   \
+    __assert_equal_private_os << #x << " != " << #y << ", " << __FILE__ << ":" \
+                              << __LINE__;                                     \
+    AssertEqual(x, y, __assert_equal_private_os.str());                        \
   }
-
 #define ASSERT(x)                                                              \
   {                                                                            \
     ostringstream os;                                                          \
